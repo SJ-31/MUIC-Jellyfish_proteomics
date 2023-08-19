a5c7a89b9de2fdaa6e63155382d8c140a430432c
Shann Chongwattananukul <shanj3131@gmail.com>
2023-08-19 11:46:52 +0700

    feat: add protein mass and length information to fasta mapping table

--- bin/fasta_table.py
+++ bin/fasta_table.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env ipython
 
 from Bio import SeqIO
+from pyteomics import mass
 import sys
 import pandas as pd
 
@@ -10,6 +11,16 @@ out = sys.argv[2]
 fasta_frame = {"id": [], "seq": []}
 for record in SeqIO.parse(db, format="fasta"):
     fasta_frame["id"].append(record.id)
-    fasta_frame["seq"].append(record.seq)
+    fasta_frame["seq"].append(str(record.seq))
 fasta_frame = pd.DataFrame(fasta_frame)
+
+sample = fasta_frame["seq"][2]
+def mass_calc(seq) -> float:
+    if (intersect := {"X","B", "Z"} & set(seq)):
+        for i in intersect:
+            seq = seq.replace(i, "")
+    return mass.fast_mass(seq)
+
+fasta_frame["mass"] = fasta_frame["seq"].apply(mass_calc)
+fasta_frame["length"] = fasta_frame["seq"].apply(len)
 fasta_frame.to_csv(out, sep="\t", index=None)
