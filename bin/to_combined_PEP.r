eef3acd6b05f2f1c5e058283af6f7bd927f9b0dc
Shann Chongwattananukul <shanj3131@gmail.com>
2023-08-21 05:54:59 +0700

    chore: Remove modifications for peptide sequences in combined PEP file
    This should ensure that the same peptide is correctly unified between
    different engines

--- bin/to_combined_PEP.r
+++ bin/to_combined_PEP.r
@@ -9,11 +9,11 @@ parser <- add_option(parser, c("-m", "--matches"), type="character",
 parser <- add_option(parser, c("-d", "--decoys"), type="character",
                 help="File containing decoy matches only")
 parser <- add_option(parser, c("-p", "--protein_matches"), default = FALSE,
-                     action="store_true",
-                     help="Set this if the files contain matches to
+                     action = "store_true",
+                     help = "Set this if the files contain matches to
         proteins rather than peptides/psms")
 parser <- add_option(parser, c("-o", "--output"), type="character",
-                     help="Output file name")
+                     help = "Output file name")
 
 read_percolator <- function(filename, header, col_select, is_decoy) {
   percolator_output <- read.table(filename, sep = "\t", fill = TRUE,
@@ -24,14 +24,26 @@ read_percolator <- function(filename, header, col_select, is_decoy) {
     select(all_of(col_select)) %>%
     filter(!(grepl("[A-Za-z]", V2))) %>%
     filter(V2 != "") %>%
-    `colnames<-`(header)%>%
-    mutate("Is_decoy" = is_decoy)
+    `colnames<-`(header) %>%
+    mutate("Is_decoy" = is_decoy) %>%
+    mutate(peptide = unlist(lapply(peptide, gsub, pattern = "\\[.*\\]",
+                                   replacement = ""))) %>%
+    mutate(peptide = unlist(lapply(peptide, gsub, pattern = "\\.",
+                                   replacement = ""))) %>%
+    mutate(peptide = unlist(lapply(peptide, gsub, pattern = "-",
+                                   replacement = "")))
   return(percolator_output)
 }
 
 args <- parse_args(parser)
 peptide_cols <- c("V2", "V3", "V4", "V5")
 protein_cols <- c("V1", "V2", "V3", "V4")
+
+# Testing purposes
+## args <- list(matches = "../results/test_2combine/comet_percolator_psms.tsv",
+##              decoys = "../results/test_2combine/comet_percolator_decoy_psms.tsv",
+##              protein_matches = FALSE,
+##              output = "../results/test_2combine/2combined.tsv")
 
 psm_header <- c("score", "q-value", "PEP", "peptide")
 percolator_protein_header <- c("ProteinId", "ProteinGroupID", "q-value", "PEP")
