2c8479cf6caeeac75f9cd547bf78be53057a57ec
Shann Chongwattananukul <shanj3131@gmail.com>
2023-08-21 18:20:41 +0700

    feat: add formatting for metamorpheus

--- bin/flashlfq_format.r
+++ bin/flashlfq_format.r
@@ -4,50 +4,87 @@ library(optparse)
 library(glue)
 
 ## parser <- OptionParser()
-## parser <- add_option(parser, c("--metamorpheus"), type="character",
-##                 help="metamorpheus psms")
-## parser <- add_option(parser, c("--identipy"), type="character",
-##                 help="identipy psms")
-## parser <- add_option(parser, c("--maxquant"), type="character",
-##                      help = "maxquant psms")
-## parser <- add_option(parser, c("--msfragger"), type="character",
-##                      help = "msfragger psms")
-## parser <- add_option(parser, c("--comet"), type="character",
-##                      help = "comet psms")
-## parser <- add_option(parser, c("--tide"), type="character",
-##                      help = "tide psms")
-## parser <- add_option(parser, c("-o", "--output"), type="character",
-##                      help = "Output file name")
-## parser <- add_option(parser, c("-m", "--msms_mapping"), type="character",
-##                      help = "MsMs mapping file")
+## parser <- add_option(parser, c("--metamorpheus"),
+##   type = "character",
+##   help = "metamorpheus psms"
+## )
+## parser <- add_option(parser, c("--identipy"),
+##   type = "character",
+##   help = "identipy psms"
+## )
+## parser <- add_option(parser, c("--maxquant"),
+##   type = "character",
+##   help = "maxquant psms"
+## )
+## parser <- add_option(parser, c("--msfragger"),
+##   type = "character",
+##   help = "msfragger psms"
+## )
+## parser <- add_option(parser, c("--comet"),
+##   type = "character",
+##   help = "comet psms"
+## )
+## parser <- add_option(parser, c("--tide"),
+##   type = "character",
+##   help = "tide psms"
+## )
+## parser <- add_option(parser, c("-o", "--output"),
+##   type = "character",
+##   help = "Output file name"
+## )
+## parser <- add_option(parser, c("-m", "--msms_mapping"),
+##   type = "character",
+##   help = "MsMs mapping file"
+## )
 ## args <- parse_args(parser)
-##
 
-comet_input <- "../results/test_manifest/1-First_pass/Percolator/comet_percolator_psms.tsv"
 msms_ref <- "../results/CiCs_metrics.tsv"
 # One of msfragger, comet, maxquant, identipy, metamorpheus, tide
 
 group_prot <- function(prot_vec) {
-  grouped <- paste0(prot_vec, collapse = ";")
+  grouped <- paste0(prot_vec, collapse = "|")
   split_groups <- grouped %>%
-    strsplit(";") %>%
+    strsplit("|") %>%
     unlist(use.names = FALSE) %>%
     unique()
-  return(paste0(split_groups, collapse = ";"))
+  return(paste0(split_groups, collapse = "|"))
 }
 
 get_percolator_row <- function(row_index, percolator_lines) {
-    splits <- percolator_lines[row_index] %>% str_split("\t") %>% unlist()
-    return(tibble(PSMId = splits[1],
-            peptide = splits[5],
-            `Protein Accession` = group_prot(splits[6: length(splits)])))
+  splits <- percolator_lines[row_index] %>%
+    str_split("\t") %>%
+    unlist()
+  return(tibble(
+    PSMId = splits[1],
+    peptide = splits[5],
+    `Protein Accession` = group_prot(splits[6:length(splits)])
+  ))
 }
 
 read_percolator <- function(percolator_file) {
   lines <- read_lines(percolator_file)
   p_tibble <- lapply(seq_along(lines)[-1], get_percolator_row,
-                       percolator_lines = lines) %>% bind_rows()
+    percolator_lines = lines
+  ) %>% bind_rows()
   return(p_tibble)
+}
+
+read_metamorpheus <- function(metamorpheus_file) {
+    old_names <- c(
+    "File.Name", "Scan.Retention.Time", "Precursor.Charge",
+    "Base.Sequence", "Full.Sequence", "Peptide.Monoisotopic.Mass",
+    "Protein.Accession"
+    )
+    new_names <- lapply(old_names, gsub,
+    pattern = "\\.",
+    replacement = " "
+    ) %>% unlist(use.names = FALSE)
+    mm <- read.delim(metamorpheus_file, sep = "\t") %>%
+    as_tibble() %>%
+    filter(Decoy == "N") %>%
+    select(all_of(old_names)) %>%
+    rename_with(~new_names, all_of(old_names))
+    return(mm)
 }
 
 ##  Functions for formatting scan number
@@ -70,53 +107,59 @@ msfragger_scans <- function(msfragger_id_str) {
   return(groups[2])
 }
 
-
 get_file_name <- function(scan) {
   return(gsub("\\..*", "", scan))
 }
 
 clean_peptide <- function(modified_pep) {
   return(str_extract_all(modified_pep, "[A-Z]+")[[1]] %>%
-         paste0(collapse = ""))
+    paste0(collapse = ""))
 }
 
 mapping <- read.delim(msms_ref, sep = "\t")
 
 read_engine_psms <- function(percolator_input, engine) {
+  if (engine == "metamorpheus") {
+    return(read_metamorpheus(percolator_input))
+  } else if (engine == "tide") {
+  }
   # Read the percolator psm file from <engine> and format the
   #     the output according to flashlfq
   #     This relies on all functions defined above
   psms <- read_percolator(percolator_input)
-  if ( engine == "comet" ) {
-  psms <- psms %>% mutate(scan = unlist(lapply(PSMId, comet_scans)))
-  } else if ( engine == "maxquant" ) {
-  psms <- psms %>% mutate(scan = unlist(lapply(PSMId, maxquant_scans)))
-  } else if ( engine == "msfragger" ) {
-  psms <- psms %>% mutate(scan = unlist(lapply(PSMId, msfragger_scans)))
-  } else if ( engine == "tide" ) {
-
-  } else if ( engine == "metamorpheus" ) {
-
+  if (engine == "comet") {
+    psms <- psms %>% mutate(scan = unlist(lapply(PSMId, comet_scans)))
+  } else if (engine == "maxquant") {
+    psms <- psms %>% mutate(scan = unlist(lapply(PSMId, maxquant_scans)))
+  } else if (engine == "msfragger") {
+    psms <- psms %>% mutate(scan = unlist(lapply(PSMId, msfragger_scans)))
+  } else if (engine == "identipy") {
+    psms <- psms %>% mutate(scan = PSMId)
   }
   ## Nothing to do for identipy
   ## TODO: Add formmatting for metamorpheus
   ## TODO: Add formmatting for tide
   psms <- psms %>%
-  mutate(`File Name` = unlist(lapply(scan, get_file_name))) %>%
-  mutate(`Full Sequence` = peptide) %>%
-  mutate(`Base Sequence` = unlist(lapply(peptide, clean_peptide)))%>%
-  mutate(`Peptide Monoisotopic Mass` = unlist(
-          lapply(`Base Sequence`, mw, monoisotopic = TRUE))) %>%
-    select(c("File Name", "scan", "Base Sequence", "Full Sequence",
-             "Peptide Monoisotopic Mass",
-             "Protein Accession"))
+    mutate(`File Name` = unlist(lapply(scan, get_file_name))) %>%
+    mutate(`Full Sequence` = peptide) %>%
+    mutate(`Base Sequence` = unlist(lapply(peptide, clean_peptide))) %>%
+    mutate(`Peptide Monoisotopic Mass` = unlist(
+      lapply(`Base Sequence`, mw, monoisotopic = TRUE)
+    )) %>%
+    select(c(
+      "File Name", "scan", "Base Sequence", "Full Sequence",
+      "Peptide Monoisotopic Mass",
+      "Protein Accession"
+    ))
   new_cols <- c("Scan Retension Time", "Precursor Charge")
   old_cols <- c("retensionTime", "precursorCharge")
   psms <- inner_join(psms, mapping, by = join_by(scan == scanNum)) %>%
-    rename_with( ~new_cols, all_of(old_cols)) %>%
-    select(c("File Name", "Scan Retension Time", "Precursor Charge",
-             "Base Sequence", "Full Sequence", "Peptide Monoisotopic Mass",
-             "Protein Accession"))
+    rename_with(~new_cols, all_of(old_cols)) %>%
+    select(c(
+      "File Name", "Scan Retension Time", "Precursor Charge",
+      "Base Sequence", "Full Sequence", "Peptide Monoisotopic Mass",
+      "Protein Accession"
+    ))
   return(psms)
 }
 
@@ -124,9 +167,27 @@ read_engine_psms <- function(percolator_input, engine) {
 ##                   tide = args$tide, metamorpheus = args$metamorpheus,
 ##                   msfragger = args$msfragger, maxquant = args$maxquant)
 
-file_list <- list(comet = "../results/test_manifest/1-First_pass/Percolator/comet_percolator_psms.tsv",
-identipy = "../results/test_manifest/1-First_pass/Percolator/identipy_percolator_psms.tsv", maxquant =  "../results/test_manifest/1-First_pass/Percolator/maxquant_percolator_psms.tsv", msfragger = "../results/test_manifest/1-First_pass/Percolator/msfragger_percolator_psms.tsv")
+file_list <- list(
+  comet = "../results/test_manifest/1-First_pass/Percolator/comet_percolator_psms.tsv",
+  identipy = "../results/test_manifest/1-First_pass/Percolator/identipy_percolator_psms.tsv", maxquant = "../results/test_manifest/1-First_pass/Percolator/maxquant_percolator_psms.tsv", msfragger = "../results/test_manifest/1-First_pass/Percolator/msfragger_percolator_psms.tsv"
+)
 
-unique_peps <- vector()
-comet <- read_engine_psms(comet_input, "comet")
-unique_peps <- append(unique_peps, comet$`Base Sequence`)
+
+flashlfq <- read_engine_psms(file_list[[1]], names(file_list)[1])
+# Set up the first set of psms
+unique_peps <- first$`Base Sequence`
+#
+# Add only new psms to the first set
+for (n in names(file_list)[-1]) {
+    current <- read_engine_psms(file_list[[n]], names(file_list)[n])
+    current_peps <- second$`Base Sequence`
+    new_rows <- current %>% filter(!(`Base Sequence` %in% unique_peps))
+    flashlfq <- bind_rows(flashlfq, new_rows)
+    unique_peps <- c(unique_peps,
+                        current_peps[!(current_peps %in% unique_peps)])
+    rm(current, current_peps, new_rows)
+}
+
+read_metamorpheus <- function(mm_file) {
+  read
+}
