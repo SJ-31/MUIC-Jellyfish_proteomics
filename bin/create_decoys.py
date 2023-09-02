e5ca5aabba786f895f725e2ae7bb67a30cbd5315
Shann Chongwattananukul <shanj3131@gmail.com>
2023-09-02 15:50:53 +0700

    feat: Add separation of downloaded and de novo + transcriptome proteins

--- bin/create_decoys.py
+++ bin/create_decoys.py
@@ -3,27 +3,49 @@ import sys
 import pandas as pd
 from Bio import SeqIO
 
+
 def write_lines(file: str, line_list: list) -> None:
     with open(file, "a") as a:
         for line in line_list:
             a.write(line)
 
-mapping: dict = {"key": [], "val": []}
+
+input = sys.arv[1]
 mapping_name = sys.argv[2]
+# input = "./all.fasta"
+# mapping_name = "./headers.tsv"
+
+mapping: dict = {"key": [], "val": []}
 combined: str = "decoysWnormal.fasta"
 decoys: str = "all_decoys.fasta"
+downloaded: str = "downloaded.fasta"
 normal: str = "all_normal.fasta"
-for num, record in enumerate(SeqIO.parse(sys.argv[1], "fasta")):
-    mapping["key"].append(f"P{num}")
+num_download = 0
+num_other = 0
+is_download = True
+for record in SeqIO.parse(input, "fasta"):
+    if ("-DENOVO" in record.id or "-TRANSCRIPTOME" in record.id):
+        seq_id = f"O{num_other}"
+        num_other += 1
+        is_download = False
+    else:
+        seq_id = f"P{num_download}"
+        num_download += 1
+        is_download = True
+    mapping["key"].append(seq_id)
     mapping["val"].append(f"{record.description}")
     if "J" in record.seq or "U" in record.seq:
         seq = str(record.seq).replace("J", "").replace("U", "")
     else:
         seq = str(record.seq)
-    norm_lines: list = [f">P{num}" + "\n", str(seq) + "\n"]
-    decoy_lines: list = [f">rev_P{num}" + "\n", str(seq[::-1]) + "\n"]
+    norm_lines: list = [f">{seq_id}" + "\n", str(seq) + "\n"]
+    decoy_lines: list = [f">rev_{seq_id}" + "\n", str(seq[::-1]) + "\n"]
     write_lines(normal, norm_lines)
     write_lines(decoys, decoy_lines)
     write_lines(combined, norm_lines + decoy_lines)
+    if is_download:
+        write_lines(downloaded, norm_lines)
+        is_download = False
+
 mapping = pd.DataFrame(mapping)
 mapping.to_csv(mapping_name, sep="\t", header=False, index=False)
--- modules/combined_database.nf
+++ modules/combined_database.nf
@@ -1,17 +1,19 @@
 process COMBINED_DATABASE {
     publishDir "$outdir", mode: "copy"
-    publishDir "$projectDir", mode: "copy", pattern: "*.DB.txt"
+    publishDir "$text_loc", mode: "copy", pattern: "*.DB.txt"
 
     input:
     path(other_fasta)
     path(denovo_peptides)
     val(outdir)
+    val(text_loc)
     //
 
     output:
     path("decoysWnormal.fasta")
     path("all_normal.fasta")
     path("all_decoys.fasta")
+    path("blast_db")
     path("*tsv")
     path("${params.pref}.DB.txt"), emit: listing
     //
@@ -26,6 +28,15 @@ process COMBINED_DATABASE {
     fasta_table.py all_normal.fasta all_normal_mapping.tsv
     find . \\( -name "*fasta" -o -name "*tsv" \\) -type f > temp_list.txt
     cat temp_list.txt | sed 's;./;!{outdir}/;' > !{params.pref}.DB.txt
+
+    mv downloaded.fasta temp; cat temp | seqkit rmdup > downloaded.fasta
+    makeblastdb -in downloaded.fasta \
+        -parse_seqids \
+        -blastdb_version 5 \
+        -title Cnidarian_proteins \
+        -dbtype prot \
+        -out downloaded
+    mkdir blast_db; mv downloaded* blast_db
     '''
     //
 }
