99629fd4c2e6057992cf87f29c1160024c4c9fbe
Shann Chongwattananukul <shanj3131@gmail.com>
2023-12-19 18:03:43 +0700

    chore: merge open searches with other search results

--- bin/merge_quantifications.py
+++ bin/merge_quantifications.py
@@ -46,15 +46,16 @@ def split_proteins(row):
 
 
 def merge_dlfq(direct_lfq, prot_df, pep_thresh, fdr):
-    direct_lfq["protein"] = clean_list_col("protein", direct_lfq,
+    dlfq_copy = direct_lfq.copy()
+    dlfq_copy["protein"] = clean_list_col("protein", dlfq_copy,
                                            ";").apply(list)
-    direct_lfq = direct_lfq[direct_lfq["protein"].map(lambda x: len(x) > 0)]
-    split = [split_proteins(row[1]) for row in direct_lfq.iterrows()]
-    direct_lfq = pd.concat(split)
-    direct_lfq = direct_lfq[~direct_lfq["protein"].str.contains("rev_")]
-    direct_lfq = direct_lfq.groupby("protein").aggregate(np.median)
+    dlfq_copy = dlfq_copy[dlfq_copy["protein"].map(lambda x: len(x) > 0)]
+    split = [split_proteins(row[1]) for row in dlfq_copy.iterrows()]
+    dlfq_copy = pd.concat(split)
+    dlfq_copy = dlfq_copy[~dlfq_copy["protein"].str.contains("rev_")]
+    dlfq_copy = dlfq_copy.groupby("protein").aggregate(np.median)
     merged = pd.merge(prot_df,
-                      direct_lfq,
+                      dlfq_copy,
                       how="left",
                       left_on="ProteinId",
                       right_on="protein")
@@ -73,6 +74,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--direct_lfq")
     parser.add_argument("-i", "--intersected_searches")
+    parser.add_argument("-s", "--open_searches")
     parser.add_argument("-p", "--pep_threshold")
     parser.add_argument("-q", "--fdr_threshold")
     parser.add_argument("-o", "--output")
@@ -80,12 +82,52 @@ def parse_args():
     return args
 
 
-if __name__ == '__main__':
-    args = parse_args()
+def concatComma(df, col1, col2) -> pd.Series:
+    """
+    Concatenate columns col1 and col2 such that their entries are
+    joined end-to-end in a csv list.
+    """
+    return df[col1].combine(df[col2], lambda x,y: f"{x},{y}")
+
+
+def main(args: dict):
     dlfq = read_directlfq(args["direct_lfq"])
     proteins = pd.read_csv(args["intersected_searches"], sep="\t")
+    open_searches = pd.read_csv(args["open_searches"], sep="\t")
+    open_searches.rename({"q-value": "q.value"}, axis="columns", inplace=True)
+
+    # Identify proteins that were found in both standard and open search
+    shared = proteins.merge(open_searches.filter(["ProteinId", "peptideIds",
+                                                  "q.value",
+                                                  "posterior_error_prob"]),
+                            on="ProteinId",
+                            how="inner")
+    shared["peptideIds"] = concatComma(shared, "peptideIds_x", "peptideIds_y")
+    shared["q.value"] = concatComma(shared, "q.value_x", "q.value_y")
+    shared["posterior_error_prob"] = concatComma(shared,
+                                                 "posterior_error_prob_x",
+                                                 "posterior_error_prob_y")
+    shared.drop(["peptideIds_x", "peptideIds_y", "posterior_error_prob_x",
+                 "posterior_error_prob_y", "q.value_x", "q.value_y"],
+                inplace=True, axis="columns")
+
+    # Isolate proteins found only in open search and standard search
+    proteins = proteins[~proteins["ProteinId"].isin(shared["ProteinId"])]
+    open_searches = (open_searches[~open_searches["ProteinId"]
+                                   .isin(shared["ProteinId"])])
+
+    # Merge with quantification
     merged = merge_dlfq(dlfq, proteins, float(args["pep_threshold"]),
                         float(args["fdr_threshold"]))
-    # qdf = calculate_emPAI(merged, mapping, (350, 1600))
-    merged["Annotation_method"] = "initial_database"
-    merged.to_csv(args["output"], sep="\t", index=False)
+    merged_shared = merge_dlfq(dlfq, shared, float(args["pep_threshold"]),
+                        float(args["fdr_threshold"]))
+    merged_open = merge_dlfq(dlfq, open_searches, float(args["pep_threshold"]),
+                        float(args["fdr_threshold"]))
+    final = pd.concat([merged, merged_shared, merged_open])
+    final["Anno_method"] = "initial_database"
+    final.to_csv(args["output"], sep="\t", index=False)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
