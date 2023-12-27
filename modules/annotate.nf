3207c7bd6c29f9f27cc8a2cf43564f917cc6323f
Shann Chongwattananukul <shanj3131@gmail.com>
2023-12-27 12:15:32 +0700

    feat: finalize db annotation module

--- modules/annotate.nf
+++ modules/annotate.nf
@@ -1,5 +1,7 @@
 process ANNOTATE {
     publishDir "$outdir", mode: "copy", pattern: "{*.tsv,*.fasta}"
+    publishDir "$outdir", mode: "copy", pattern: "${name}_eggnog-unmatched"
+    publishDir "$outdir", mode: "copy", pattern: "${name}_interpro-unmatched"
     // May need to install pandas and requests
 
     input:
@@ -9,34 +11,12 @@ process ANNOTATE {
 
     output:
     tuple path("${name}_anno.tsv"), path("${name}_meta.tsv"), emit: annotations
+    path("${name}_eggnog-unmatched")
+    path("${name}_interpro-unmatched")
     path("*fasta"), optional: true
     //
 
     shell:
     name = combined_tsv.baseName.replaceFirst( /db_hits-/, "")
-    check = file("${outdir}/${name}_anno.tsv")
-    if (check.exists()) {
-        '''
-        cp !{outdir}/!{name}_*.tsv .
-        '''
-    } else {
-        template 'annotate.sh'
-    }
-    // need_anno = file("needs_annotating.fasta")
-    // Some NCBI proteins fail to map to UniProt, so these are extracted,
-    // annotated with InterProscan, then merged back into the original
-    // annotations file
-    // if (need_anno.exists()) {
-    //     '''
-    //     header='query\tsequence_md5\tlength\tmember_db\tdb_accession\tdescription\tstart\tstop\tevalue\tstatus\tdate\tinterpro_accession\tinterpro_description\tGO\tpathways'
-    //     annotate.py -s \
-    //         -u \
-    //         -i annotated.tsv \
-    //         -m !{name}_meta.tsv \
-    //         -a !{name}_anno.tsv
-    //     '''
-        // need_anno = file("still_needs_annotating.fasta")
-        // if (need_anno.exists()) {
-
-        // }
+    template 'annotate.sh'
 }
--- modules/templates/annotate.sh
+++ modules/templates/annotate.sh
@@ -6,22 +6,30 @@ annotate.py -i !{combined_tsv} \
 
 eggdir="!{name}_eggnog-unmatched"
 ipdir="!{name}_interpro-unmatched"
+previous_eggnog="!{outdir}/${eggdir}"
+previous_interpro="!{outdir}/${ipdir}"
 
 if [ -e needs_annotating.fasta ]; then
+    if [ ! -d "${previous_eggnog}" ]; then
     mkdir ${eggdir}; mv needs_annotating* ${eggdir}; cd ${eggdir}
     export EGGNOG_DATA_DIR=!{params.eggnog_data_dir}
     conda run -n eggnog emapper.py -i needs_annotating.fasta \
         --output temp \
         --report_orthologs
+    else
+        cp -r "${previous_eggnog}" .
+        cd ${eggdir}
+    fi
 
     Rscript !{params.bin}/sort_eggnog.r \
-        -f needs_annotating2.fasta \
+        --output_fasta needs_annotating2.fasta \
         --blast needs_annotating.tsv \
         --output_unmatched !{name}_eggnog_unmatched.tsv \
         --output_meta !{name}_meta-eggnog.tsv \
         --output_anno !{name}_anno-eggnog.tsv \
-        -seeds temp.emapper.seed_orthologs \
-        -annotations temp.emapper.annotations
+        --seeds temp.emapper.seed_orthologs \
+        --annotations temp.emapper.annotations
+    mv needs_annotating2.fasta ..
     # No need to get the sequences from the eggnog db, because
     # this is dealing with full-length proteins, not peptides
     cd ..
@@ -31,21 +39,25 @@ if [ -e needs_annotating.fasta ]; then
         --eggnog_anno_tsv ${eggdir}/!{name}_anno-eggnog.tsv
 
     if [ -e needs_annotating2.fasta ]; then
-        interpro_header='query\tsequence_md5\tlength\tmember_db\tdb_accession\tdescription\tstart\tstop\tevalue\tstatus\tdate\tinterpro_accession\tinterpro_description\tGO\tpathways'
-        mkdir ${ipdir}; mv needs_annotating2.fasta ${ipdir}; cd ${ipdir}
-        interproscan.sh -i needs_annotating2.fasta \
-            -f tsv \
-            -goterms \
-            -pa \
-            -b temp
-        echo -e "$header" > header.txt
-        cat header.txt temp.tsv > annotated.tsv
-        rm temp.tsv
-        cd ..
+        if [ ! -d "${previous_interpro}" ]; then
+            interpro_header='query\tsequence_md5\tlength\tmember_db\tdb_accession\tdescription\tstart\tstop\tevalue\tstatus\tdate\tinterpro_accession\tinterpro_description\tGO\tpathways'
+            mkdir ${ipdir}; mv needs_annotating2.fasta ${ipdir}; cd ${ipdir}
+            interproscan.sh -i needs_annotating2.fasta \
+                -f tsv \
+                -goterms \
+                -pa \
+                -b temp
+            echo -e "$interpro_header" > header.txt
+            cat header.txt temp.tsv > annotated.tsv
+            rm temp.tsv
+            cd ..
+        else
+            cp -r "${previous_interpro}" .
+        fi
 
         annotate.py --merge_interpro \
             --interpro_query ${ipdir}/needs_annotating2.fasta \
-            -r $params.bin \
+            -r !{params.bin} \
             -i ${ipdir}/annotated.tsv \
             --anno_tsv !{name}_anno.tsv
         mv sorted.tsv ${ipdir}
