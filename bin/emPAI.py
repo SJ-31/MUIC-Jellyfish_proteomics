8e6f19d11a77cffeb419ee33c79b3a8d2977192e
Shann Chongwattananukul <shanj3131@gmail.com>
2024-01-01 20:02:50 +0700

    feat: update emPAI for use with reticulate

--- bin/emPAI.py
+++ bin/emPAI.py
@@ -6,7 +6,7 @@ from pyteomics import mass
 
 def clean_pep(peptide):
     peptide = peptide.upper()
-    return ''.join(re.findall("[^BXZ]+", peptide))
+    return "".join(re.findall("[^BXZ]+", peptide))
 
 
 def n_observed(pep_list):
@@ -16,38 +16,40 @@ def n_observed(pep_list):
     return len(peps)
 
 
-def n_observable(protein_id, mapping, mass_range):
-    try:
-        seq = mapping.loc[protein_id]["seq"]
-    except KeyError:
-        print("Id not found")
-        return None
-    pep_df = pd.DataFrame({"obsv_pep": list(parser.cleave(seq, 'trypsin'))})
+def n_observable(seq, mass_range):
+    pep_df = pd.DataFrame({"obsv_pep": list(parser.cleave(seq, "trypsin"))})
     pep_df["obsv_pep"] = pep_df["obsv_pep"].apply(clean_pep)
-    pep_df["mass"] = (
-        pep_df["obsv_pep"].apply(lambda x: mass.calculate_mass(sequence=x)))
-    pep_df["In_range"] = (
-        pep_df["mass"].apply(lambda x: mass_range[0] <= x <= mass_range[1]))
+    pep_df["mass"] = pep_df["obsv_pep"].apply(
+        lambda x: mass.calculate_mass(sequence=x)
+    )
+    pep_df["In_range"] = pep_df["mass"].apply(
+        lambda x: mass_range[0] <= x <= mass_range[1]
+    )
     if pep_df["In_range"].any():
-        return (pep_df["In_range"].value_counts()[True])
+        return pep_df["In_range"].value_counts()[True]
     return None
 
 
 def emPAI(observed, observable):
-    return 10**(observed / observable) - 1
+    return 10 ** (observed / observable) - 1
 
 
-def calculate_emPAI(df, mapping, m_range):
-    not_full = df["header"].str.contains("-DENOVO|-TRANSCRIPTOME", regex=True)
+def calculate_emPAI(df, m_range: tuple):
+    not_full = (
+        df["header"]
+        .str.contains("-DENOVO|-TRANSCRIPTOME", regex=True)
+        .fillna(False)
+        .combine(df["seq"] == "-", lambda x, y: x or y)
+    )
     full = df[~not_full]
     other = df[not_full]
-    mapping.index = mapping["id"]
-    full["observable_peps"] = full["ProteinId"].apply(n_observable,
-                                                      mapping=mapping,
-                                                      mass_range=m_range)
+    full["observable_peps"] = full["seq"].apply(
+        n_observable, mass_range=m_range
+    )
     full["observed_peps"] = full["peptideIds"].apply(n_observed)
     full["emPAI"] = full.apply(
-        lambda x: emPAI(x.observed_peps, x.observable_peps), axis=1)
+        lambda x: emPAI(x.observed_peps, x.observable_peps), axis=1
+    )
     full = full.drop(["observable_peps", "observed_peps"], axis="columns")
     df = pd.concat([full, other])
     return df
