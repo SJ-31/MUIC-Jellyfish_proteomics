122311b47c1a99dbefa52e0bcda0d7290a17551b
Shann Chongwattananukul <shanj3131@gmail.com>
2024-01-14 14:06:26 +0700

    chore: adjust peptide cleaning for emPAI script

--- bin/emPAI.py
+++ bin/emPAI.py
@@ -4,9 +4,12 @@ from pyteomics import parser
 from pyteomics import mass
 
 
-def clean_pep(peptide):
+def cleanPep(peptide):
     peptide = peptide.upper()
-    return "".join(re.findall("[^BXZ]+", peptide))
+    peptide = "".join(re.findall("[^BXZ]+", peptide))  # Leaving these
+    # unknown residues will raise an error with emPAI
+    peptide = "".join(re.findall("[A-Z]+", peptide))
+    return peptide
 
 
 def n_observed(pep_list):
@@ -17,8 +20,8 @@ def n_observed(pep_list):
 
 
 def n_observable(seq, mass_range):
+    seq = cleanPep(seq)
     pep_df = pd.DataFrame({"obsv_pep": list(parser.cleave(seq, "trypsin"))})
-    pep_df["obsv_pep"] = pep_df["obsv_pep"].apply(clean_pep)
     pep_df["mass"] = pep_df["obsv_pep"].apply(
         lambda x: mass.calculate_mass(sequence=x)
     )
@@ -42,6 +45,9 @@ def calculate_emPAI(df, m_range: tuple):
         .combine(df["seq"] == "-", lambda x, y: x or y)
     )
     full = df[~not_full & df["seq"].notna()]
+    full["peptideIds"] = full["peptideIds"].apply(
+        lambda x: ",".join([cleanPep(p) for p in x.split(",")])
+    )
     other = df[not_full | df["seq"].isna()]
     full["observable_peps"] = full["seq"].apply(
         n_observable, mass_range=m_range
@@ -53,8 +59,3 @@ def calculate_emPAI(df, m_range: tuple):
     full = full.drop(["observable_peps", "observed_peps"], axis="columns")
     df = pd.concat([full, other])
     return df
-
-
-test = pd.read_csv(
-    "./results/jellyfish/1-First_pass/jellyfish_all.tsv", sep="\t"
-)
