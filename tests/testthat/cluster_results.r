c8850da6606035e8730254dae7304e43ebd37884
Shann Chongwattananukul <shanj3131@gmail.com>
2024-04-09 09:39:14 +0700

    chore: update tests

--- tests/testthat/cluster_results.r
+++ tests/testthat/cluster_results.r
@@ -1,5 +1,32 @@
 library(tidyverse)
 library(fpc)
 
-res <- read_tsv("./cluster_stats.tsv")
-leiden <- read_tsv("./leiden_metrics.tsv")
+res <- read_tsv("./tests/testthat/cluster_stats.tsv")
+leiden <- read_tsv("./tests/testthat/leiden_metrics.tsv")
+
+sortHelper <- function(metric, tb, ascending = TRUE) {
+  selected <- select(tb, all_of(c(metric, "method", "parameter")))
+  if (ascending) {
+    return(arrange(selected, !!as.symbol(metric)))
+  }
+  return(arrange(selected, desc(!!as.symbol(metric))))
+}
+
+# Metrics to maximize
+dunn <- sortHelper("dunn", res, ascending = FALSE)
+dunn2 <- sortHelper("dunn2", res, ascending = FALSE)
+# Average silhouette score
+avg_sil <- sortHelper("avg.silwidth", res, ascending = FALSE)
+
+# Correlation between distances and a 0-1-vector where 0 means same cluster,
+# 1 means different clusters
+pearsongamma <- sortHelper("pearsongamma", res, ascending = FALSE)
+
+
+# Metrics to minimize
+# The average distance within clusters
+average_within <- sortHelper("average.within", res)
+# The widest distance within clusters
+widest_within <- sortHelper("widestgap", res)
+# Ratio of average within-cluster distances / average between-cluster distances
+wb_ratio <- sortHelper("wb.ratio", res)
