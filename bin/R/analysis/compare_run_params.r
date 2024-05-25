dfa256ea5171db1adc6f2e0cb2516d2eaebb5e0f
Shann Chongwattananukul <shanj3131@gmail.com>
2024-05-25 15:49:13 +0700

    feat: add tests for de novo vs no denovo

--- bin/R/analysis/compare_run_params.r
+++ bin/R/analysis/compare_run_params.r
@@ -1,9 +1,10 @@
-library(tidyverse)
-library(ggplot2)
-library(ggridges)
-library(venn)
-library(Peptides)
-library(glue)
+library("tidyverse")
+library("ggVennDiagram")
+library("ggplot2")
+library("ggridges")
+library("venn")
+library("Peptides")
+library("glue")
 if (str_detect(getwd(), "Bio_SDD")) {
   wd <- "/home/shannc/Bio_SDD/MUIC_senior_project/workflow"
   env <- "/home/shannc/Bio_SDD/miniconda3/envs/reticulate"
@@ -29,7 +30,6 @@ source(glue("{args$r_source}/analysis/metric_functions.r"))
 FDR <- 0.05
 PATH <- glue("{wd}/results/")
 
-
 runs <- list("C_indra.calibrated", "C_indra.msconvert", "C_indra", "ND_C_indra")
 names(runs) <- c("msgf_calibrated", "peak_picked", "default", "no_denovo")
 data <- lapply(names(runs), \(x) {
@@ -46,13 +46,9 @@ TABLES <- list()
 
 # Intersection of protein_ids
 id_list <- lapply(data, \(x) x$ProteinId)
-
-protein_intersection <- venn(id_list,
-  plotsize = 100, borders = FALSE, box = FALSE,
-  ilcs = 1, sncs = 1.5,
-  ilabels = "counts",
-  zcolor = "red, blue, green, white, orange, yellow"
-)
+venn <- ggVennDiagram(id_list, label_alpha = 0) +
+  scale_fill_gradient(low = "#eff1f5", high = "#40a02b")
+GRAPHS$run_param_venn <- venn
 
 getCommonColumn <- function(tb_list, target_col, predicate) {
   lapply(tb_list, \(x) {
@@ -107,8 +103,6 @@ GRAPHS$unique_peptide_number <- unique_peptide_number$graph +
 
 #' ---------------------------------------------------
 # Analyze GO and KEGG pathway composition
-
-
 GO_list <- lapply(data, \(x) {
   list(GO_IDs = goVector(x, go_column = "GO_IDs"), GO_slims = goVector(x, go_column = "GO_slims"))
 })
@@ -126,6 +120,10 @@ go_slims <- local({
   filter(!if_all(where(is.numeric), is.na)) %>%
   mutate(across(is.numeric, \(x) replace(x, is.na(x), 0)))
 
+paramXSlim <- tbTranspose(go_slims)
+dist <- vegan::vegdist(paramXSlim, method = "bray")
+pcoa <- vegan::wcmdscale(dist, eig = TRUE)
+# pcoa$points %>% ggplot(aes(x = ))
 
 go_terms <- local({
   tbs <- lmap(GO_list, \(x) {
@@ -135,3 +133,61 @@ go_terms <- local({
   purrr::reduce(tbs, \(acc, x) left_join(acc, x), .init = tibble(GO_IDs = unique_terms))
 }) %>%
   mutate(across(is.numeric, \(x) replace(x, is.na(x), 0)))
+
+#' ----------
+#' Evaluate contribution of denovo peptides
+merged <- inner_join(data$default, data$no_denovo,
+  by = join_by(ProteinId),
+  suffix = c(".def", ".nd")
+)
+coverage <- list(default = merged$pcoverage_align.def, no_denovo = merged$pcoverage_align.nd)
+
+compare_cols <- c("pcoverage_nmatch", "pcoverage_align", "num_unique_peps", "num_peps")
+merged <- merged %>% mutate(across(contains(compare_cols), as.numeric))
+tests <- pairwiseFromTb(
+  merged,
+  c("pcoverage_nmatch", "pcoverage_align", "num_unique_peps", "num_peps"),
+  rep("greater", 4),
+  \(x, y, ...) wilcox.test(x, y, paired = TRUE, ...),
+  suffixes = c(".def", ".nd")
+)
+
+replaceSuffix <- function(name_list, replacement_map) {
+  map_chr(name_list, \(x) str_replace_all(x, replacement_map))
+}
+
+coverage_list <- merged %>%
+  select(contains("coverage")) %>%
+  as.list()
+names(coverage_list) <- replaceSuffix(
+  names(coverage_list),
+  c(".def" = " default", ".nd" = " no denovo")
+)
+
+peps_list <- merged %>%
+  select(contains("peps")) %>%
+  as.list()
+names(peps_list) <- replaceSuffix(
+  names(peps_list),
+  c(
+    ".def" = " default", ".nd" = " no denovo",
+    "num_peps" = "", "num_unique_peps" = "unique"
+  )
+)
+
+GRAPHS$default_no_denovo_coverage <- ggplotNumericDist(coverage_list, method = "freq_poly", linewidth = 0.9) +
+  xlab("Alignment coverage (%)") +
+  ylab("Frequency") +
+  labs(
+    title = "Coverage comparison between runs with and without added de novo peptides",
+    color = "Parameter"
+  )
+GRAPHS$default_no_denovo_coverage
+
+GRAPHS$default_no_denovo_peptide_count <- ggplotNumericDist(peps_list, method = "boxplot") +
+  xlab("Run parameter") +
+  ylab("Count") +
+  labs(
+    title = "Number of identified peptides between runs with and without added de novo peptides",
+    color = "Parameter"
+  )
