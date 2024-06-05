a48c6d532853e98cd7be81699f93f8e3cd5976d8
Shann Chongwattananukul <shanj3131@gmail.com>
2024-06-05 09:51:20 +0700

    feat: add function to map GOs to parents in main results

--- bin/go_subset.py
+++ bin/go_subset.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python
+import sys
 import networkx as nx
 import json
-import sys
-import argparse
 import polars as pl
 from collections import ChainMap
 import obonet
@@ -74,24 +74,31 @@ class SubsetGO:
             .sort("n_children", descending=True)
         )
 
-    def __checkRepresentative(self, to_check: str, representatives: list):
-        """Make sure the roots do not contain each other"""
-        return all([to_check not in self.successors[e] for e in representatives])
+    def __checkParent(self, to_check: str, ancestors: list):
+        """Make sure the parent terms do not contain each other"""
+        return all([to_check not in self.successors[e] for e in ancestors])
 
-    def getRepresentatives(
-        self, ontology: str, n: int = 18, show=True, min_depth=2, pre=tuple()
-    ):
-        """Select representative GO terms from the specified ontology that partition the ontology into `n` bins.
-        Goal is to map all child terms below the representatives to each representative,
-        making for more concise summarization
-        representative terms are selected by the number of children they have, as
-        well as the specified depth. Or can be pre-specified with the `pre` argument
+    def getPredefined(self, path: str, ontology: str) -> dict:
+        with open(path, "r") as r:
+            custom = json.load(r)
+        id2group = {}
+        reference = self.metadata.filter(pl.col("ontology") == ontology)
+        for group, members in custom.items():
+            for member in members:
+                if member in reference["GO_IDs"]:
+                    id2group[member] = group
+        return id2group
+
+    def getParents(self, ontology: str, n: int = 18, show=True, min_depth=2, pre=""):
+        """Select higher-level parent GO terms from the specified ontology that partition the ontology into `n` bins.
+        Goal is to map all child terms to some higher-level term to make for more concise summarization
+        higher-level terms are selected by the number of children they have, as well as the specified depth. They can also be pre-specified with the `pre` argument
 
         :return: A dictionary of the following
-        `map`: Map of GO terms in the specified sub-ontology to the representatives above them
-        `representatives`: Map of chosen representatives to their terms
-        `unassigned`: GO terms in the sub-ontology graph that are not children of
-        any representative. Happens when representatives have few children
+        `map`: Map of GO terms in the specified sub-ontology to their assigned higher-level terms
+        `parents`: Map of chosen parents to their terms
+        `unassigned`: GO terms in the sub-ontology graph that are not children of any of the chosen parents. Happens when chosen parents have few children
+        `pre`: path to a json file containing pre-defined groups (mapping a GO id or group name to specific terms) that will override other mappings
         """
         children_per_cat = self.metadata.shape[0] / n
         filtered = self.metadata.filter(
@@ -100,29 +107,27 @@ class SubsetGO:
                 & (pl.col("n_children") <= children_per_cat)
                 & (pl.col("level") >= min_depth)
             )
-            | (pl.col("GO_IDs").is_in(pre))
         ).sort("n_children", descending=True)
-        representatives = pre if pre else []
+        parents: list = []
         for go_id, *_ in filtered.iter_rows():
-            if self.__checkRepresentative(go_id, representatives):
-                representatives.append(go_id)
-            if len(representatives) == n:
+            if self.__checkParent(go_id, parents):
+                parents.append(go_id)
+            if len(parents) == n:
                 break
-        representative_df = filtered.filter(pl.col("GO_IDs").is_in(representatives))
+        parent_df = filtered.filter(pl.col("GO_IDs").is_in(parents))
         if show:
-            print(representative_df)
-        child_to_parent: dict = {}
+            print(parent_df)
+        child_to_parent: dict = self.getPredefined(pre, ontology) if pre else {}
         unassigned: set = set(filtered["GO_IDs"])
-        for representative in representatives:
-            for child in self.successors[representative]:
-                child_to_parent[child] = representative
-            child_to_parent[representative] = representative
+        for parent in parents:
+            for child in self.successors[parent]:
+                if child not in child_to_parent:
+                    child_to_parent[child] = parent
+            child_to_parent[parent] = parent
         unassigned = unassigned - child_to_parent.keys()
         return {
             "map": child_to_parent,
-            "representatives": dict(
-                zip(representative_df["GO_IDs"], representative_df["term"])
-            ),
+            "parents": dict(zip(parent_df["GO_IDs"], parent_df["term"])),
             "unassigned": unassigned,
         }
 
@@ -177,28 +182,172 @@ def levelMap(G: nx.DiGraph, root=None) -> dict:
 # Group and find the lowest common ancestors???
 # current = GoSubDag(["GO:0031577"], path)
 # current.rcntobj.go2ancestors["GO:0031577"]
-def parseArgs():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-g", "--go_path")
-    parser.add_argument("-s", "--sample_path")
-    parser.add_argument("-p", "--go_info_path")
-    parser.add_argument("-n", "--n_groups")
-    parser.add_argument("--representatives")
-    parser.add_argument("-o", "--outdir")
-    args = vars(parser.parse_args())
-    return args
 
 
-def main(args):
-    representatives = {"CC": None, "BP": None, "MF": None}
-    S = SubsetGO(args["go_path"], args["sample_path"], args["go_info_path"])
-    for sub in representatives.keys:
-        rep = S.getRepresentatives()
-        representatives[sub] = {"map": rep["map"], "unassigned": rep["unassigned"]}
-    with open(f'{args["outdir"]}/go_representatives.json', "w") as j:
-        json.dump(representatives, j)
+def toJson(
+    go_path: str,
+    sample_path: str,
+    go_info_path: str,
+    outdir: str,
+    n_groups: int,
+    predefined: str = None,
+):
+    data = {"CC": None, "BP": None, "MF": None}
+    S = SubsetGO(go_path, sample_path, go_info_path)
+    for sub in data.keys():
+        rep = S.getParents(sub, n=n_groups, pre=predefined)
+        data[sub] = {
+            "map": rep["map"],
+            "unassigned": list(rep["unassigned"]),
+        }
+    with open(f"{outdir}/go_parents.json", "w") as j:
+        json.dump(data, j)
 
 
-if __name__ == "__main__" and not "ipykernel" in sys.argv[0]:
-    args = parseArgs()
-    main(args)
+def intoOntology(gos: list, mapping: dict) -> dict:
+    partitioned: dict = {"CC": [], "BP": [], "MF": []}
+    for go in gos:
+        lookup: str = mapping[go]
+        if lookup != "NA":  # Check if term is obsolete
+            partitioned[lookup].append(go)
+    return partitioned
+
+
+def parentGOs(
+    source: str,
+    go_terms: list,
+    rep_map: dict,
+    term_map: dict,
+    ontology_map: dict,
+    priority: list = None,
+) -> dict:
+    """Obtain higher level parent GO terms from a term list (i.e. terms of a protein)
+
+    Args:
+        source (string): where do these ids come from?
+        go_terms (list): list of GO ids to get higher-level terms from
+        rep_map (dict): mapping of GO ids to their representative parent terms
+        ontology_map (dict): map of GO ids to their sub-ontology
+        ontology_map (dict): map of GO ids to their English terms
+    """
+    grouped = intoOntology(go_terms, ontology_map)
+    terms = pl.DataFrame()
+    missing = {"CC": 0, "BP": 0, "MF": 0}
+    found_special = False
+    for o in missing.keys():
+        cur_group = grouped[o]
+        reduced = {}
+        cur_map = rep_map[o]
+        for id in cur_group:
+            found = cur_map["map"].get(id)
+            if found and priority and found in priority:
+                reduced[found] = sys.maxsize
+                found_special = True
+            elif found:
+                reduced[found] = reduced.get(found, 0) + 1
+            else:
+                reduced[id] = 1
+        if not reduced:
+            continue
+        temp = (
+            pl.DataFrame(reduced)
+            .melt()
+            .rename({"variable": "GO_IDs", "value": "count"})
+            .with_columns(ontology=pl.lit(o))
+            .sample(n=len(reduced), shuffle=True)
+            .sort("count", descending=True)
+        )
+        # Shuffling (using sample) is just a precaution in case all the entries have the same "count"
+        terms = pl.concat([terms, temp], how="vertical")
+    if terms.is_empty():
+        # print(f"Warning: obsolete terms {list(go_terms)}")
+        return {"CC": "unknown", "BP": "unknown", "MF": "unknown"}, missing
+    aggregated = terms.group_by("ontology", maintain_order=True).agg(
+        pl.col("GO_IDs").first()
+    )
+    result = dict(zip(aggregated["ontology"], aggregated["GO_IDs"]))
+    if found_special:
+        print(result)
+    for k in missing.keys():
+        v = result.get(k)
+        if not v:
+            result[k] = "unknown"
+            # print(f"Warning: {source} lacks terms from {k}")
+            missing[k] = 1
+            continue
+        cur_map = rep_map[k]
+        if (v not in cur_map["map"] or v in cur_map["unassigned"]) and (
+            not priority or v not in priority
+        ):
+            result[k] = "other"
+        else:
+            if ":" in v:  # Check that the key is actually a GO id, and
+                # not one of the user-defined groups
+                result[k] = term_map[v]
+            else:
+                print(f"from end loop {v}")
+                result[k] = v
+    return result, missing
+
+
+def parentsFromDf(
+    combined_results: str, go_info_path: str, parents_path: str, priority_path: str = ""
+) -> tuple[pl.DataFrame, pl.DataFrame]:
+    """Find higher-level GO terms of identified proteins in a results file
+
+    Args:
+        combined_results (str): path to results file
+        go_info_path (str): path to file containing GO term metadata
+        parents_path (str): path to json file mapping GO ids to their assigned parent terms
+        output (str): output file name
+    """
+    info: pl.DataFrame = pl.read_csv(go_info_path, separator="\t")
+    id2ontology = dict(zip(info["GO_IDs"], info["ontology"]))
+    id2term = dict(zip(info["GO_IDs"], info["term"]))
+    data = pl.read_csv(combined_results, separator="\t", null_values="NA")
+    has_go = data.filter(pl.col("GO_IDs").is_not_null())
+    no_go = data.filter(pl.col("GO_IDs").is_null())
+    if priority_path:
+        with open(priority_path, "r") as j:
+            priority = list(json.load(j).keys())
+    else:
+        priority = []
+    with open(parents_path, "r") as j:
+        rmap: dict = json.load(j)
+    id_go: pl.DataFrame = (
+        has_go.select("ProteinId", "GO_IDs", "GO_counts")
+        .with_columns(
+            GO_IDs=pl.col("GO_IDs").map_elements(
+                lambda x: x.split(";"), return_dtype=pl.List(pl.Utf8)
+            )
+        )
+        .sort("GO_counts", descending=True)
+    )
+    cc, bp, mf = [], [], []
+    missing_counts = {"CC": 0, "BP": 0, "MF": 0}
+    for id, gos in zip(id_go["ProteinId"], id_go["GO_IDs"]):
+        parents, missing = parentGOs(
+            id, gos, rmap, id2term, id2ontology, priority=priority
+        )
+        cc.append(parents["CC"])
+        bp.append(parents["BP"])
+        mf.append(parents["MF"])
+        for k, v in missing.items():
+            missing_counts[k] += v
+    added = has_go.with_columns(
+        GO_category_CC=pl.Series(cc),
+        GO_category_MF=pl.Series(mf),
+        GO_category_BP=pl.Series(bp),
+    )
+    no_go = no_go.with_columns(
+        GO_category_CC=pl.lit("unknown"),
+        GO_category_MF=pl.lit("unknown"),
+        GO_category_BP=pl.lit("unknown"),
+    )
+    result = pl.concat([added, no_go], how="vertical")
+    missing_df = (
+        pl.DataFrame(missing_counts)
+        .melt()
+        .rename({"variable": "ontology", "value": "n_missing"})
+    )
+    return result.to_pandas(), missing_df.to_pandas()
