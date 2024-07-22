2c1a909dd8cd291bb0c4289569092e1576a2b230
Shann Chongwattanankul <shanj3131@gmail.com>
2024-07-22 10:36:40 +0700

    chore: look at peptides for bias, not proteins

--- bin/R/analysis/subanalyses/engine_category_bias.r
+++ bin/R/analysis/subanalyses/engine_category_bias.r
@@ -2,6 +2,8 @@ if (!exists("SOURCED")) {
   source(paste0(dirname(getwd()), "/", "all_analyses.r"))
   SOURCED <- TRUE
 }
+ta <- new.env()
+reticulate::source_python(glue("{M$python_source}/trace_alignments.py"), envir = ta)
 
 GRAPHS <- list()
 TABLES <- list()
@@ -33,40 +35,61 @@ engine_counts <- num_ids$ProteinGroupId %>%
   discard(\(x) x == "U") %>%
   table()
 
-compare_col <- "GO_category_MF"
-engine_hits <- tb %>%
-  select(ProteinId, ProteinGroupId) %>%
-  apply(1, \(x) {
-    groups <- split_group_str(
-      x["ProteinGroupId"],
-      TRUE, TRUE
-    )
-    row <- tibble(ProteinId = x["ProteinId"])
-    for (g in groups) {
-      row <- add_column(row, !!g := TRUE)
-    }
-    return(row)
-  }) %>%
-  bind_rows() %>%
-  replace_na_all(., FALSE)
+#' Assign the values of `vec` into three levels: "low", "medium", "high"
+#' based on their locations in the distribution of vec
+#'
+#' @description
+#' low = below 1st quartile
+#' medium = betweeen 2nd and 3rd quartiles
+#' high = above 3rd
+levels_by_quartile <- function(vec) {
+  quartiles <- summary(vec)
+  case_when(
+    vec < quartiles[["1st Qu."]] ~ "low",
+    quartiles[["3rd Qu."]] < vec ~ "high",
+    .default = "medium"
+  )
+}
+summary(hits$length)[["1st Qu."]]
+
+compare_col <- "length_category"
+engine_hits <- ta$get_peptide_match_df(
+  M$peptide_map_path,
+  M$aq_reformat_path
+) |> as_tibble()
+ENGINES <- names(engine_counts)
+
+engine_hits <- engine_hits |> mutate(length_category = levels_by_quartile(engine_hits$length))
 hits <- engine_hits
 
+# Logistic regression
+# Given a peptide is present, do these variables affect whether or not they will
+# be hit?
+tide_logit <- glm(tide ~ mass + length, data = hits, family = "binomial")
+summary(tide_logit)
+
 # Collapse the contingency table
+engine_hits
 engine_hits <- engine_hits %>%
-  inner_join(., dplyr::select(tb, !!compare_col, ProteinId)) %>%
-  select(-ProteinId) %>%
+  select(-PeptideId) %>%
   filter(!is.na(!!compare_col)) %>%
   group_by(!!as.symbol(compare_col)) %>%
   filter(n() > 30) %>%
   ungroup()
 
-categories <- unique(engine_hits[[compare_col]])
+# Compute effect size for significant hits, using the odds ratio
+# This represents how much more likely the engine is to identify
+# peptides from a protein of the given category
+# Confidence interval is 95%
+length_tests <- chisqNME(engine_hits, ENGINES, compare_col, "engine", "peptide length")
+
 
 # An example of what the contingency table for the tests would look like,
 # and what the expected values would be under the chi-square test of independence
+categories <- unique(engine_hits[[compare_col]])
 current <- "comet"
 tl <- table(engine_hits[[compare_col]], engine_hits[[current]])
-ct <- "protein binding"
+ct <- "low"
 show_contigency <- table(engine_hits[[compare_col]] == ct, engine_hits[[current]])
 expected <- format_engine_contingency(show_contigency, ct, TRUE)
 expected %>%
@@ -77,101 +100,6 @@ expected %>%
   )
 TABLES$sample_expected <- expected
 
-engines <- colnames(engine_hits)[!colnames(engine_hits) %in% c("U", compare_col)]
-engine_chi <- tibble()
-engine_table_list <- list()
-for (engine in engines) {
-  engine_table_list[[engine]] <- list()
-  for (cat in categories) {
-    t <- table(engine_hits[[compare_col]] == cat, engine_hits[[engine]])
-    engine_table_list[[engine]][[cat]] <- t
-    tst <- chisq.test(t)
-    row <- tibble(engine = engine, category = cat, p_value = tst$p.value)
-    engine_chi <- bind_rows(engine_chi, row)
-  }
-}
-engine_chi <- engine_chi %>% mutate(
-  p_adjust = p.adjust(p_value, "holm"),
-  is_significant = ifelse(p_value < 0.05, TRUE, FALSE)
-)
-
-# Compute effect size for significant hits, using the odds ratio
-# This represents how much more likely the engine is to identify
-# peptides from a protein of the given category
-# Confidence interval is 95%
-odds_ratios <- engine_chi %>%
-  apply(1, \(x) {
-    row <- tibble(OR = NA, OR_upper_ci = NA, OR_lower_ci = NA)
-    if (!as.logical(x["is_significant"])) {
-      return(row)
-    }
-    e <- x["engine"]
-    c <- x["category"]
-    table <- engine_table_list[[e]][[c]]
-    try({
-      odds_ratio <- get_odds_ratio(table)
-      odds_ratio_ci_u <- get_odds_ratio(table, TRUE, "upper")
-      odds_ratio_ci_l <- get_odds_ratio(table, TRUE, "lower")
-      row <- tibble(
-        OR = odds_ratio,
-        OR_upper_ci = odds_ratio_ci_u,
-        OR_lower_ci = odds_ratio_ci_l
-      )
-    })
-    return(row)
-  }) %>%
-  bind_rows()
-engine_chi_f <- bind_cols(engine_chi, odds_ratios)
-TABLES$engine_chi <- engine_chi_f %>%
-  mutate(across(is.double, \(x) round(x, 4))) %>%
-  mutate(
-    OR = paste0(OR, " [", OR_lower_ci, ", ", OR_upper_ci, "]"),
-    OR = map_chr(OR, \(x) ifelse(grepl("NA|NaN", x), "NA", x))
-  ) %>%
-  select(-matches("OR_")) %>%
-  rename("Odds ratio, 95% CI [lower, upper]" = OR) %>%
-  gt() %>%
-  tab_header(
-    title = "Association between engine hits and protein GO category",
-    subtitle = "Tested using chi-square"
-  )
-# Odds ratio is interpreted as the given engine is OR times as likely to miss
-# the protein when it is NOT of the stated category than when it is
-# So higher OR means that the engine is biased towards hitting proteins in that category
-
-
-# Record all results
-engine_table_tb <- tibble(category = categories)
-remove_cat <- FALSE
-for (e in names(engine_table_list)) {
-  e_list <- engine_table_list[[e]]
-  temp_lst <- list()
-  remove_col_label <- FALSE
-  for (ct in names(e_list)) {
-    tib <- format_engine_contingency(e_list[[ct]], w_expected = TRUE)
-    if (remove_cat) {
-      tib <- dplyr::select(tib, -category)
-    }
-    tib <- gt(tib)
-    if (!remove_col_label) {
-      remove_col_label <- TRUE
-    } else {
-      tib <- tib %>% tab_options(column_labels.hidden = TRUE)
-    }
-    temp_lst <- c(temp_lst, as_raw_html(tib))
-  }
-  temp_tb <- tibble(!!e := temp_lst)
-  engine_table_tb <- bind_cols(engine_table_tb, temp_tb)
-  remove_cat <- TRUE
-}
-TABLES$engine_category_contingency <- engine_table_tb %>%
-  gt() %>%
-  fmt_markdown(columns = everything()) %>%
-  tab_header(
-    title = md("**Contingency tables used in chi-square analysis of engine bias**"),
-    subtitle = "For each table, misses are on the left, hits on the right.
-Expected values are in parentheses"
-  )
 
 
 # Correlation between no. identifications by engines and coverage
@@ -198,6 +126,7 @@ intensity$intensity_class <- map_chr(intensity$log_intensity, \(x) {
     "second"
   }
 })
+
 hits <- hits |> inner_join(intensity, by = join_by(ProteinId))
 
 intense <- chisqNME(
