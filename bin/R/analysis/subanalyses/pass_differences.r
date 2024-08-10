aaf28e3fe74c29e301dca19ce72751e3a46e6a01
Shann Chongwattanankul <shanj3131@gmail.com>
2024-08-10 22:25:52 +0700

    chore: trace origin of peptides in 2nd pass

--- bin/R/analysis/subanalyses/pass_differences.r
+++ bin/R/analysis/subanalyses/pass_differences.r
@@ -1,4 +1,3 @@
-# BUG Don't really need this one tbh
 if (!exists("SOURCED")) {
   source(paste0(dirname(getwd()), "/", "all_analyses.r"))
   SOURCED <- TRUE
@@ -7,18 +6,18 @@ SECOND_PASS_ENGINES <- c("identipy", "msgf", "msfragger", "comet")
 
 TABLES <- list()
 
-db <- glue("{M$path}/Databases/decoysWnormal.fasta")
+db <- glue("{M$chosen_path}/Databases/decoysWnormal.fasta")
 db_size <- function(fasta_file) {
   seqkit_stat(fasta_file)$num_seqs[[1]] %>% as.numeric()
 }
 
 complete_db_size <- db_size(db)
 sizes <- lapply(SECOND_PASS_ENGINES, \(x) {
-  db_size(glue("{M$path}/2-Second_pass/BK_databases/{x}_bk_database.fasta"))
+  db_size(glue("{M$chosen_path}/2-Second_pass/BK_databases/{x}_bk_database.fasta"))
 }) %>% `names<-`(SECOND_PASS_ENGINES)
 
 get_percolator <- function(pass) {
-  dir <- glue("{M$path}/{pass}/Percolator")
+  dir <- glue("{M$chosen_path}/{pass}/Percolator")
   lapply(SECOND_PASS_ENGINES, \(x) {
     read_tsv(glue("{dir}/{x}_percolator_proteins.tsv"))
   }) %>% `names<-`(SECOND_PASS_ENGINES)
@@ -44,8 +43,9 @@ get_stats <- function(engine_list, pass) {
 
 
 stats <- bind_rows(get_stats(first_pass, "first"), get_stats(sec_pass, "sec"))
-SEQ_MAP <- read_tsv(glue("{M$path}/Databases/seq-header_mappings.tsv"))
+SEQ_MAP <- read_tsv(glue("{M$chosen_path}/Databases/seq-header_mappings.tsv"))
 
+NEW_IN_SEC <- c()
 
 ALL_RESULTS <- bind_rows(
   mutate(M$run$first, pass = "first"),
@@ -94,7 +94,9 @@ per_engine <- function(engine) {
   data <- mutate(data, lost_in_sec = !header %in% sec$header)
 
 
-  new_in_sec <- sec %>% filter(!header %in% first$header) # Should be empty
+  lost_in_sec <- filter(first, !ProteinId %in% sec$ProteinId)
+  print(all(sec$header %in% first$header))
+  NEW_IN_SEC <<- c(NEW_IN_SEC, filter(sec, !header %in% first$header) |> pluck("ProteinId")) # Should be empty
 
   found_in_both <- inner_join(first, sec,
     by = join_by(x$header == y$header),
@@ -109,6 +111,7 @@ per_engine <- function(engine) {
     delta_q_value = `q-value.sec` - `q-value.first`,
     delta_pep = posterior_error_prob.sec - posterior_error_prob.first
   )
+  # Want to check the number of peptides in the proteins lost
 
   # Run pairwise tests and get summary statistics to check
   # if the second pass performed better than the first
@@ -137,6 +140,7 @@ per_engine <- function(engine) {
     \(x) {
       data$lost_in_sec
       table <- table(data$from != x, data$lost_in_sec)
+      print(table)
       or <- table %>% get_odds_ratio()
       upper <- table %>% get_odds_ratio(CI = TRUE)
       lower <- table %>% get_odds_ratio(CI = TRUE, side = "lower")
@@ -148,8 +152,13 @@ per_engine <- function(engine) {
   # second pass is OR times as high in proteins of the current group than
   # all others
 
+  # Change to check peptides
   # Check if the lengths of proteins lost have a statistically significant difference
   # with those kept
+  as_peps <- data |>
+    mutate(peptideIds = map_chr(peptideIds, fill_peptide_gaps)) |>
+    separate_longer_delim("peptideIds", ";") |>
+    mutate(length = nchar(peptideIds))
   lengths_lost <- data %>%
     filter(lost_in_sec) %>%
     purrr::pluck("length")
@@ -158,25 +167,18 @@ per_engine <- function(engine) {
     purrr::pluck("length")
 
   test <- wilcox.test(lengths_lost, lengths_kept)
-  test$data.name <- "Lengths of proteins lost in second pass vs lengths of those retained"
+  test$data.name <- "lost x retained"
   p <- test$`p.value`
   test <- htest2tb(test)
   if (p < 0.05) {
     t2 <- wilcox.test(lengths_lost, lengths_kept, alternative = "less")
-    t2$data.name <- "Lengths of proteins lost in second pass vs lengths of those retained"
-    t2$alternative <- "lengths of lost proteins less than kept"
-    p <- t2$`p.value`
-    if (p >= 0.05) {
-      t2 <- wilcox.test(lengths_lost, lengths_kept, alternative = "greater")
-      t2$data.name <- "Lengths of proteins lost in second pass vs lengths of those retained"
-      t2$alternative <- "Lengths of lost proteins greater than kept"
-    }
+    t2$data.name <- "lost x retained"
+    t2$alternative <- "lost less"
     test <- bind_rows(test, htest2tb(t2))
   }
   engine_results$htests <- bind_rows(chi, test)
   return(engine_results)
 }
-
 
 
 TABLES <- purrr::reduce(SECOND_PASS_ENGINES, \(acc, x) {
@@ -210,6 +212,12 @@ pw$conclusion <- pmap(
 
 TABLES$pairwise_tests_raw <- pw
 
+sub <- substitute_all(
+  c("num_peps", "pcoverage_align"),
+  c("peptide number", "percent coverage"),
+  \(x) gsub("_", " ", x)
+)
+
 TABLES$pairwise_tests <- pw %>%
   filter(two_sided_significant == "Y") |>
   filter(metric == "num_peps") |>
@@ -242,6 +250,54 @@ TABLES$pairwise_tests <- pw %>%
   fmt_number(., columns = contains("p-value"), decimals = 5) %>%
   tab_stubhead(label = "Metric")
 
+
 TABLES$stats <- stats
 
-save(TABLES, glue("{M$outdir}/pass_differences"))
+# ----------------------------------------
+# Confirm that new proteins were seen previously
+get_blast <- function(pass, prefix = M$chosen_prefix, path = M$chosen_path) {
+  bind_rows(
+    read_tsv(glue("{path}/{pass}/Unmatched/BLAST/{prefix}_blast_matched.tsv")),
+    read_tsv(glue("{path}/{pass}/Unmatched/BLAST/{prefix}_blast_unmatched.tsv"))
+  )
+}
+
+new_in_sec <- M$run$second |>
+  filter(!header %in% M$run$first$header) |>
+  distinct(ProteinId, .keep_all = TRUE) |>
+  pluck("ProteinId")
+bmatches <- get_blast("1-First_pass")
+bmatches2 <- get_blast("2-Second_pass")
+
+b_ids <- c(bmatches$ProteinId, flatten_by(bmatches$MatchedPeptideIds, ";"))
+b_ids2 <- c(bmatches2$ProteinId, flatten_by(bmatches2$MatchedPeptideIds, ";"))
+found_previously <- new_in_sec %>% keep(\(x) x %in% b_ids)
+found_in_blast <- new_in_sec %>% keep(\(x) (x %in% b_ids2) && !(x %in% found_previously))
+others <- new_in_sec |> discard(\(x) x %in% found_previously || x %in% found_in_blast)
+
+TABLES$new_in_second_pass <- glue("
+Total number of new proteins in second pass: {length(b_ids2)}
+Number found in first pass: {length(found_previously)}
+% found in first pass: {length(found_previously)/length(b_ids)}
+Number found from blast in second pass {length(found_in_blast)}
+% found from blast in second pass {length(found_in_blast)/length(b_ids2)}
+")
+
+TABLES$peptide_length_tests <- TABLES$htests |>
+  get_adjusted_p() |>
+  filter(!grepl("Chi", method)) |>
+  rename(pair = data) |>
+  mutate(pair = paste0(pair, " (", engine, ")")) |>
+  conclude_one_sided() |>
+  pairwise_conclusion2gt()
+
+GRAPHS <- list()
+GRAPHS$odds_ratio <- TABLES$OR |> ggplot(aes(x = engine, y = odds_ratio, fill = source)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  geom_errorbar(aes(ymin = CI_lower, ymax = CI_upper), width = 0.2, position = position_dodge(.9)) +
+  scale_fill_paletteer_d("awtools::ppalette") +
+  ylab("Odds of being lost in second pass") +
+  guides(fill = guide_legend("Protein type"))
+attr(GRAPHS$odds_ratio, "height") <- 6
+
+save(c(GRAPHS, TABLES), glue("{M$outdir}/pass_differences"))
