e5d0ce15f9fc0436744cf33f6075445c38ce9c86
Shann Chongwattanankul <shanj3131@gmail.com>
2024-08-14 22:35:29 +0700

    chore: add python script for de novo matches

--- docs/find_denovo_equivalents.py
+++ docs/find_denovo_equivalents.py
@@ -44,6 +44,7 @@ was_matched: set = set(
     )
 )
 
+
 perc_prot = (
     pl.read_csv(
         f"{results}/{prefixes['default']}/{chosen_pass}/percolator_all.tsv",
@@ -61,8 +62,8 @@ perc_peps = pl.read_csv(
     null_values="NA",
 ).select(perc_prot.columns)
 
-perc = pl.concat([perc_peps, perc_prot])
-
+perc: pl.DataFrame = pl.concat([perc_peps, perc_prot])
+perc.write_csv(f"{outdir}/filtered_percolator.tsv")
 
 original_denovo: pl.DataFrame = hh.fasta2df(
     f"{wd}/data/protein_databases/denovo_normal_mgf/denovo_all.fasta"
@@ -87,31 +88,6 @@ peptides["ND"] = (
 types = ["MATCHED", "ALL"]
 # Difference is that "MATCHED" uses peptides that were actually identified by engines
 
-denovo_peps: list[set] = [set(denovo["peptideIds"]), set(original_denovo["seq"])]
-for type, d in zip(types, denovo_peps):
-    # Direct instances of engine peptides being matched to de novo peptides
-    direct_matches = peptides["ND"] & d
-    hh.cat(
-        [
-            "Number of direct matches between ND engine peptides and de novo peptides",
-            len(direct_matches),
-            f"Proportion: {len(direct_matches)/len(peptides['ND'])}",
-            f"Number of total remaining ND engine peptides: {len(peptides['ND'])}",
-            f"Number of matched ND engine peptides: {number_matched_engines}",
-        ],
-        f"{outdir}/direct_matches-{type}.txt",
-    )
-
-    result_file = f"{outdir}/denovo_ND_hits-{type}.tsv"
-    if not Path(result_file).exists():
-        # Attempt to find all matched ND engine peptides in a de novo peptide
-        find_in_denovo: pl.DataFrame = pl.from_pandas(
-            hh.find_matches(peptides["ND"], d)
-        )
-        find_in_denovo.write_csv(result_file, separator="\t", null_value="NA")
-    else:
-        hits[type] = pl.read_csv(result_file, separator="\t", null_values="NA")
-
 seq_header_map = (
     pl.read_csv(
         f"{results}/C_indra/Databases/seq-header_mappings.tsv",
@@ -122,18 +98,85 @@ seq_header_map = (
     .select("id", "seq", "header")
 )
 
-for k, v in hits.items():
-    df: pl.DataFrame = (
-        v.join(seq_header_map, left_on="best_hit", right_on="seq")
-        .unique("id")
-        .with_columns(identified=pl.col("id").is_in(perc["ProteinId"]))
-        .with_columns(
-            pl.struct(["query", "best_hit"])
-            .map_elements(
-                lambda x: di.Levenshtein.distance(x["query"], x["best_hit"]),
-                return_dtype=pl.Int64,
+denovo_peps: list[set] = [set(denovo["peptideIds"]), set(original_denovo["seq"])]
+
+all_denovo = pl.concat(
+    [
+        (
+            denovo.rename({"peptideIds": "seq"})
+            .join(
+                seq_header_map.drop("seq"),
+                left_on="ProteinId",
+                right_on="id",
             )
-            .alias("distance")
-        )
+            .select(original_denovo.columns + ["ProteinId"])
+            .with_columns(from_engine_ids=pl.lit(True))
+        ),
+        original_denovo.with_columns(ProteinId=None, from_engine_ids=pl.lit(False)),
+    ]
+)
+
+# all_denovo_peps: pl.DataFrame = de
+for type, d in zip(types, denovo_peps):
+    # Direct instances of engine peptides being matched to de novo peptides
+    direct_matches = peptides["ND"] & d
+    hh.py_cat(
+        [
+            "Number of direct matches between ND engine peptides and de novo peptides",
+            len(direct_matches),
+            f"Proportion: {len(direct_matches)/len(peptides['ND'])}",
+            f"Number of total remaining ND engine peptides: {len(peptides['ND'])}",
+            f"Number of matched ND engine peptides: {number_matched_engines}",
+        ],
+        f"{outdir}/direct_matches-{type}.txt",
     )
-    df.write_csv(f"{outdir}/{k}_final.tsv", separator="\t", null_value="NA")
+
+result_file = f"{outdir}/denovo_ND_hits-COMPLETE.tsv"
+if not Path(result_file).exists():
+    # Attempt to find all matched ND engine peptides in a de novo peptide
+    find_in_denovo: pl.DataFrame = pl.from_pandas(
+        hh.find_matches(peptides["ND"], set(all_denovo["seq"]))
+    )
+    find_in_denovo.write_csv(result_file, separator="\t", null_value="NA")
+else:
+    hits = pl.read_csv(result_file, separator="\t", null_values="NA")
+
+
+df: pl.DataFrame = hits.join(
+    all_denovo, left_on="best_hit", right_on="seq"
+).with_columns(
+    pl.struct(["query", "best_hit"])
+    .map_elements(
+        lambda x: di.Levenshtein.distance(x["query"], x["best_hit"]),
+        return_dtype=pl.Int64,
+    )
+    .alias("distance")
+)
+
+df.write_csv(f"{outdir}/COMPLETE_final.tsv", separator="\t", null_value="NA")
+
+
+transcriptome: pl.DataFrame = perc.filter(pl.col("ProteinId").str.contains("T"))
+not_confident: pl.DataFrame = hits.filter(pl.col("similarity") < 0.7)
+remaining_nd = set(not_confident["query"])
+direct_matches_t = remaining_nd & set(transcriptome["peptideIds"])
+
+hh.py_cat(
+    [
+        "Number of direct matches between ND engine peptides and transcriptome peptides",
+        len(direct_matches_t),
+        f"Proportion: {len(direct_matches_t)/len(remaining_nd)}",
+        f"Number of total remaining ND engine peptides: {len(remaining_nd)}",
+    ],
+    f"{outdir}/direct_matches-TRANSCRIPTOME.txt",
+)
+# Attempt to check if remaining unconfident de novo peptides are from transcriptome
+# peptides
+result_file = f"{outdir}/denovo_ND_hits-TRANSCRIPTOME.tsv"
+if not Path(result_file).exists():
+    find_in_denovo_t: pl.DataFrame = pl.from_pandas(
+        hh.find_matches(remaining_nd, set(transcriptome["peptideIds"]))
+    )
+    find_in_denovo_t.write_csv(result_file, separator="\t", null_value="NA")
+else:
+    hits_t = pl.read_csv(result_file, separator="\t", null_values="NA")
