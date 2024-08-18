b2cd94a4a2056478681aaabd235610d362279acb
Shann Chongwattanankul <shanj3131@gmail.com>
2024-08-18 20:58:21 +0700

    chore: add new plot to fgsea

--- bin/R/analysis/fgsea.r
+++ bin/R/analysis/fgsea.r
@@ -3,6 +3,7 @@ if (!exists("SOURCED")) {
   SOURCED <- TRUE
 }
 TABLES <- list()
+GRAPHS <- list()
 CACHE <- glue("{M$wd}/.cache")
 library("enrichplot")
 
@@ -40,6 +41,17 @@ gene_sets <- list(
   toxins = toxins
 )
 
+kegg_metadata_file <- glue("{fgsea_dir}/kegg_metadata.tsv")
+if (!file.exists(kegg_metadata_file)) {
+  hh <- new.env()
+  reticulate::source_python(glue("{args$python_source}/helpers.py"), envir = hh)
+  kegg_entries <- fgsea_sig$pathway |> keep(\(x) str_detect(x, "[0-9]"))
+  kegg_metadata <- hh$get_kegg_metadata(kegg_entries) |> as_tibble()
+  write_tsv(kegg_metadata, kegg_metadata_file)
+} else {
+  kegg_metadata <- read_tsv(kegg_metadata_file)
+}
+
 pwy <- group_pathways(d$sample_tb)
 
 categories <- table(d$sample_tb$assigned_COG) %>%
@@ -59,7 +71,63 @@ if (!file.exists(fgsea_result_file)) {
     gene_sets = gene_sets
   )
 
-  fgsea_percolator_groups$fgsea$result <- fgsea_percolator_groups$fgsea$result %>% filter(padj < 0.05)
+  fgsea_sig <- fgsea_percolator_groups$fgsea$result %>% filter(padj < 0.05)
+
+  keggid2pathway_name <- tb2named_list(kegg_metadata, "entry", "name")
+  fgsea_sig <- fgsea_sig |>
+    mutate(
+      new_pathway =
+        map_chr(pathway, \(x) ifelse(x %in% names(keggid2pathway_name), keggid2pathway_name[[x]], x))
+    ) |>
+    distinct(new_pathway, .keep_all = TRUE)
+
+  renamed_groups <- local({
+    g <- fgsea_percolator_groups$groups
+    g <- g[names(g) %in% fgsea_sig$pathway]
+    names(g) <- fgsea_sig$new_pathway
+    g
+  })
+  fgsea_sig <- fgsea_sig |>
+    mutate(pathway = new_pathway) |>
+    select(-new_pathway)
+
+  top <- fgsea_sig[head(order(NES, decreasing = TRUE), n = 15), pathway]
+
+  group_tb <- named_list2tb(fgsea_percolator_groups$groups) |>
+    mutate(name = map_chr(name, \(x) lget(keggid2pathway_name, x, x)))
+
+  rank_tb <- fgsea_percolator_groups$fgsea$ranked |>
+    as_tibble() |>
+    mutate(group = names(fgsea_percolator_groups$fgsea$ranked), rank = rank(-value))
+
+  formatted <- fgsea_sig |>
+    as_tibble() |>
+    inner_join(group_tb, by = join_by(x$pathway == y$name)) |>
+    inner_join(rank_tb, by = join_by(x$value == y$group))
+
+  top_enriched <- formatted |>
+    filter(pathway %in% top) |>
+    mutate(padj = -log(padj)) |>
+    ggplot(aes(x = rank, y = NES, color = pathway, alpha = padj)) +
+    geom_point(shape = 3, size = 4) +
+    theme_bw() +
+    M$default_theme +
+    guides(
+      color = guide_legend("Protein group\n(In order of appearance from top)"),
+      alpha = guide_legend("-Log adjusted p-value"),
+    ) +
+    ylab("Normalized Enrichment Score (NES)") +
+    xlab("Intensity rank") +
+    scale_x_continuous(
+      breaks = seq(0, max(formatted$rank), by = 500)
+    ) +
+    scale_y_continuous(
+      breaks = seq(4, max(formatted$NES), by = 0.2)
+    ) +
+    scale_color_discrete(breaks = top)
+  GRAPHS$top_enriched <- top_enriched
+  attr(GRAPHS$top_enriched, "width") <- 20
+
 
   if (!dir.exists(fgsea_dir)) {
     dir.create(fgsea_dir)
@@ -67,6 +135,7 @@ if (!file.exists(fgsea_result_file)) {
 
   if (nrow(fgsea_percolator_groups$fgsea$result) != 0) {
     write_tsv(fgsea_percolator_groups$fgsea$result, glue("{fgsea_dir}/results.tsv"))
+    write_tsv(fgsea_sig, glue("{fgsea_dir}/formatted_results"))
     plots <- plotFgsea(
       fgsea_percolator_groups$groups,
       fgsea_percolator_groups$fgsea$ranked,
@@ -85,24 +154,9 @@ if (!file.exists(fgsea_result_file)) {
   fgsea_sig <- read_tsv(fgsea_result_file)
 }
 
-
-kegg_metadata_file <- glue("{fgsea_dir}/kegg_metadata.tsv")
-if (!file.exists(kegg_metadata_file)) {
-  hh <- new.env()
-  reticulate::source_python(glue("{args$python_source}/helpers.py"), envir = hh)
-  kegg_entries <- fgsea_sig$pathway |> keep(\(x) str_detect(x, "[0-9]"))
-  kegg_metadata <- hh$get_kegg_metadata(kegg_entries) |> as_tibble()
-  write_tsv(kegg_metadata, kegg_metadata_file)
-} else {
-  kegg_metadata <- read_tsv(kegg_metadata_file)
-}
 kegg_sig <- fgsea_sig |> inner_join(kegg_metadata, by = join_by(x$pathway == y$entry))
-
-d$sample_tb |>
-  filter(ProteinId %in% gene_sets$ko05130) |>
-  see()
 
 TABLES$fgsea_sig_gt <- fgsea_sig |> gt()
 
 
-save(TABLES, fgsea_dir)
+save(c(TABLES, GRAPHS), fgsea_dir)
